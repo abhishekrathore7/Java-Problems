471c5c0db2bfe85575f20f782dd9855a41c12113
Aruneet Roy Chowdhury <aruneet2015@gmail.com>
2020-10-28 22:33:32 +0530

    Create Check Palindrome.java

---
+++ Check Palindrome.java
@@ -0,0 +1,23 @@
+public class CheckPalindrome {
+
+	
+	public static String reverseEachWord(String input) {
+		
+
+        String outputString = "";
+        int index = 0;
+        
+        for(int i = 0; i< input.length(); i++){
+            if(input.charAt(i) == ' '){
+                index = i+ 1;
+                outputString = outputString + input.charAt(i);
+            }else{
+                   outputString = outputString.substring(0, index) + input.charAt(i) + outputString.substring(index);
+            }
+            
+               
+        }
+             return outputString;
+        
+	}
+}
